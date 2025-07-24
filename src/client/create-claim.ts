4111a3e13b27f62fb4d7382c471db9e109bc7852
Richard <richardliang2015@u.northwestern.edu>
2025-07-24 18:06:53 -0400

    add longer timeout

--- src/client/create-claim.ts
+++ src/client/create-claim.ts
@@ -179,7 +179,7 @@ async function _createClaimOnAttestor<N extends ProviderName>(
 				// finished writing the response
 				setTimeout(() => {
 					endedHttpRequest?.()
-				}, 1000)
+				}, 3000)
 			}
 		},
 		onClose(err) {
